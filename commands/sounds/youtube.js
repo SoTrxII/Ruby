3f7c7a4b7cf1c08655064613168b2e0fca2c6d62
Lucas <lucas@localhost.localdomain>
2018-01-29 20:46:27 +0100

    Debug de la fonction youtube  - Ajout du support du parsing direct des urls sans passer par l'API youtube. les urls de videos avec liste marchent
     - Correction d'une erreur si la recherche ne retournait rien

--- commands/sounds/youtube.js
+++ commands/sounds/youtube.js
@@ -5,12 +5,14 @@ const Ytdl = require('ytdl-core');
 const YouTube = require('youtube-node');
 const youTube = new YouTube();
 
+const Utilities = require(Path.join(global.baseAppDir, 'lib','utilities.js'));
+
 //Constants
 const baseUrl = 'https://youtube.com/watch?v=';
 const specialSearchs = {
     "NyaNya's theme": "J"
 };
-const streamOptions = {volume: 0.03, passes: 1};
+const streamOptions = {seek: 0, volume: 1};
 
 //Internal Librairies
 const Log = require(Path.join(global.baseAppDir, 'lib', 'logger.js'));
@@ -24,45 +26,76 @@ youTube.setKey("AIzaSyDLxs-tX86li5_i42cWI0-0kTwR8jBF7V4");
 let playFromYoutube = (evt, command, cmdArg) => {
 
     return new Promise((resolve, reject) => {
-        if (global.voice.dispatcher) {
-            global.voice.dispatcher.end("Interrupted");
+      if (global.voice.dispatcher) {
+            global.voice.dispatcher.destroy();
+
         }
 
-        youTube.search(cmdArg, 1, {type: 'video'}, function (error, result) {
-            if (error) {
-                reject(error);
-            } else {
-                if(!result.items){
-                  evt.reply("Aucun résultat pour cette recherche");
-                  return;
-                }
-                let stream = Ytdl(baseUrl + result.items[0].id.videoId, {
-                    filter: 'audioonly', quality: 'highest'
-                });
+        //Regex matching Youtube URL. Args are :
+        // match  1 : https://
+        // match 2 : www.
+        // match 3 : youtube.com // youtu.be
+        // match 4 : videoId
+        let regYtUrl = RegExp('^(https?\:\/\/)?(www\.)?(youtube\.com|youtu\.?be)\/.*v=(.[^&]+).*$');
+        let matches = cmdArg.match(regYtUrl);
+        if(matches){
+          //Wait a second to ensure the stream destruction
+          Utilities.waitFor(500).then(() => {
+            playStream(evt, matches[4]).then(resolve());
+          });
+        }else{
+          youTube.search(cmdArg, 1, {type: 'video'}, function (error, result) {
+              if (error) {
+                  reject(error);
+              } else {
+                  if(!result.items){
+                    evt.reply("Aucun résultat pour cette recherche");
+                    return;
+                  }else{
+                    console.log(result);
+                    playStream(evt, result.items[0].id.videoId, result.items[0].snippet.title).then(resolve());
+                  }
 
-                global.voice.dispatcher = global.voice.connection.playStream(stream, streamOptions);
-
-                evt.channel.send(baseUrl + result.items[0].id.videoId);
-
-                global.voice.dispatcher.on('end', (reason) => {
-                    Log.debug("Fin de lecture de la vidéo Youtube : " + result.items[0].snippet.title);
-                    resolve(reason);
-                });
-
-                global.voice.dispatcher.on('error', (error) => {
-                    //global.voice.dispatcher.destroy();
-                    reject(error);
-                });
-
-                global.voice.dispatcher.on('debug', (info) => {
-                    Log.debug(info);
-                });
-            }
-        });
-
+              }
+          });
+        }
 
     });
 };
+
+
+function playStream(evt, videoId, title){
+  return new Promise((resolve, reject) => {
+    console.log(baseUrl + videoId);
+    let stream = Ytdl(baseUrl + videoId, {
+        filter: 'audioonly'
+    });
+
+    evt.channel.send(baseUrl + videoId);
+    global.voice.dispatcher = global.voice.connection.playStream(stream, streamOptions);
+
+    global.voice.dispatcher.on('end', (reason) => {
+      Log.debug("Fin de lecture de la vidéo Youtube : " + (title || "non nommée") );
+
+      resolve(reason)
+      console.log("resolved")
+
+
+    });
+
+    global.voice.dispatcher.on('error', (error) => {
+        //global.voice.dispatcher.destroy();
+        Log.error(error);
+        reject(error);
+    });
+
+    global.voice.dispatcher.on('debug', (info) => {
+        Log.debug(info);
+    });
+  });
+
+
+}
 
 exports.default = {
     yt: playFromYoutube
@@ -73,4 +106,4 @@ exports.help = {
       parameters: 'Texte ou URL',
       desc: "Cherche et joue une vidéo youtube dans le chat vocal."
     }
-};
+  };
